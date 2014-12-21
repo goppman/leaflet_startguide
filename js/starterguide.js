0ae540b0e5a9a23e28752253c3f74369987df66f
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-21 14:12:08 -0500

    update on line draw path

--- js/starterguide.js
+++ js/starterguide.js
@@ -20,45 +20,54 @@ var map = L.map('map').setView([ 39.769113, -86.158895], 11);
 
 //start line animaiton script
 //
-if (map.tap) map.tap.disable();
+  function onClick(e){
 
-// Transform the short [lat,lng] format in our
-// data into the {x, y} expected by arc.js.
-function obj(ll) { return { y: ll[0], x: ll[1] }; }
 
-for (var i = 0; i < pairs.length; i++) {
-  // Transform each pair of coordinates into a pretty
-  // great circle using the Arc.js plugin, as included above.
-  var generator = new arc.GreatCircle(
-    obj(pairs[i][0]),
-    obj(pairs[i][1]));
-    var line = generator.Arc(100, { offset: 10 });
-    // Leaflet expects [lat,lng] arrays, but a lot of
-    // software does the opposite, including arc.js, so
-    // we flip here.
-    var newLine = L.polyline(line.geometries[0].coords.map(function(c) {
-      return c.reverse();
-    }), {
-      color: '#bc141a',
-      weight: 5,
-      opacity: 1.0
-    })
-    .addTo(map);
-    var totalLength = newLine._path.getTotalLength();
-    newLine._path.classList.add('path-start');
-    // This pair of CSS properties hides the line initially
-    // See http://css-tricks.com/svg-line-animation-works/
-    // for details on this trick.
-    newLine._path.style.strokeDashoffset = totalLength;
-    newLine._path.style.strokeDasharray = totalLength;
-    // Offset the timeout here: setTimeout makes a function
-    // run after a certain number of milliseconds - in this
-    // case we want each flight path to be staggered a bit.
-    setTimeout((function(path) {
-      return function() {
-        // setting the strokeDashoffset to 0 triggers
-        // the animation.
-        path.style.strokeDashoffset = 0;
-      };
-    })(newLine._path), i * 100);
-  }
+
+      if (map.tap) map.tap.disable();
+
+      // Transform the short [lat,lng] format in our
+      // data into the {x, y} expected by arc.js.
+      function obj(ll) { return { y: ll[0], x: ll[1] }; }
+
+      for (var i = 0; i < pairs.length; i++) {
+        // Transform each pair of coordinates into a pretty
+        // great circle using the Arc.js plugin, as included above.
+        var generator = new arc.GreatCircle(
+          obj(pairs[i][0]),
+          obj(pairs[i][1]));
+          var line = generator.Arc(100, { offset: 10 });
+          // Leaflet expects [lat,lng] arrays, but a lot of
+          // software does the opposite, including arc.js, so
+          // we flip here.
+          var newLine = L.polyline(line.geometries[0].coords.map(function(c) {
+            return c.reverse();
+          }), {
+            color: '#bc141a',
+            weight: 5,
+            opacity: 1.0
+          })
+          .addTo(map);
+          var totalLength = newLine._path.getTotalLength();
+          newLine._path.classList.add('path-start');
+          // This pair of CSS properties hides the line initially
+          // See http://css-tricks.com/svg-line-animation-works/
+          // for details on this trick.
+          newLine._path.style.strokeDashoffset = totalLength;
+          newLine._path.style.strokeDasharray = totalLength;
+          // Offset the timeout here: setTimeout makes a function
+          // run after a certain number of milliseconds - in this
+          // case we want each flight path to be staggered a bit.
+          setTimeout((function(path) {
+            return function() {
+              // setting the strokeDashoffset to 0 triggers
+              // the animation.
+              path.style.strokeDashoffset = 0;
+            };
+          })(newLine._path), i * 100);
+        }
+
+
+    };
+
+    onClick();
